95746bfc97a4677e049549508d50302a54183c73
William Harrison <william@williamdavidharrison.com.au>
2024-07-15 13:00:18 +0800

    Update rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -10,6 +10,7 @@ export async function GET(context) {
         site: context.site,
         items: posts.map((post) => ({
             ...post.data,
+            pubDate: new Date(post.data.published),
             link: `/${post.slug}`,
         })),
     });
