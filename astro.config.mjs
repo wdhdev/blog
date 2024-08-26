fbb8464bcdac2c637618cb9694f63a300bed2470
William Harrison <william@williamdavidharrison.com.au>
2024-08-26 12:56:23 +0800

    update links

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://blog.william.net.au",
+    site: "https://blog.wdharrison.com",
     integrations: [mdx(), sitemap()],
 });
