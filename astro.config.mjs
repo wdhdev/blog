ac098870e95a10a3dade4e4086c594333f22a8ca
William Harrison <87287585+wdhdev@users.noreply.github.com>
2025-06-04 16:39:54 +0800

    change domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://blog.wharrison.com.au",
+    site: "https://wharrison.dev",
     integrations: [mdx(), sitemap()],
 });
