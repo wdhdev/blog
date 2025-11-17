a5f6a95dc3b2db02a3ea590e4eb067e102e66030
William Harrison <87287585+wdhdev@users.noreply.github.com>
2025-11-17 12:53:49 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://wharrison.com.au/blog",
+    site: "https://blog.wharrison.com.au",
     integrations: [mdx(), sitemap()],
 });
